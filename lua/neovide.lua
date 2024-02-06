f066cc2ccad53eb871d12cff448128195c3a6594
Emanuel Pilz <emonadeo@gmail.com>
2024-02-06 09:32:10 +0100

    remove deprecated neovide options

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -6,9 +6,6 @@ vim.g.neovide_padding_bottom = 48
 vim.g.neovide_padding_right = 48
 vim.g.neovide_padding_left = 48
 
-vim.g.neovide_background_color = "#1e1e2e"
-
 vim.g.neovide_floating_blur_amount_x = 8.0
 vim.g.neovide_floating_blur_amount_y = 8.0
 vim.g.neovide_floating_shadow = false
-vim.g.neovide_transparency = 0.5
