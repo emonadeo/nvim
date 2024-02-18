8f80e98aab7845ab60192d28b6bbf11a620df8a5
Emanuel Pilz <emonadeo@gmail.com>
2024-02-19 00:45:34 +0100

    add typescript integration

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -5,7 +5,14 @@ return {
 		cmd = { "ConformInfo" },
 		opts = {
 			formatters_by_ft = {
+				css = { { "prettierd", "prettier" } },
+				html = { { "prettierd", "prettier" } },
+				javascript = { { "prettierd", "prettier" } },
+				javascriptreact = { { "prettierd", "prettier" } },
+				json = { { "prettierd", "prettier" } },
 				lua = { "stylua" },
+				typescript = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettierd", "prettier" } },
 			},
 			-- Set up format-on-save
 			format_on_save = { timeout_ms = 500, lsp_fallback = true },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,45 +1,57 @@
 return {
-	"neovim/nvim-lspconfig",
-	cmd = { "LspInfo", "LspInstall", "LspStart" },
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
-		{ "hrsh7th/cmp-nvim-lsp" },
-		{ "williamboman/mason.nvim" },
-		{ "williamboman/mason-lspconfig.nvim" },
-	},
-	config = function()
-		local lsp_zero = require("lsp-zero")
-		lsp_zero.set_server_config({
-			capabilities = {
-				workspace = {
-					didChangeWatchedFiles = {
-						dynamicRegistration = true,
+	{
+		"neovim/nvim-lspconfig",
+		cmd = { "LspInfo", "LspInstall", "LspStart" },
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+		},
+		config = function()
+			local lsp_zero = require("lsp-zero")
+			lsp_zero.set_server_config({
+				capabilities = {
+					textDocument = {
+						foldingRange = {
+							dynamicRegistration = false,
+							lineFoldingOnly = true,
+						},
+					},
+					workspace = {
+						didChangeWatchedFiles = {
+							dynamicRegistration = true,
+						},
 					},
 				},
-			},
-		})
-		lsp_zero.extend_lspconfig()
-		lsp_zero.on_attach(function(_, bufnr)
-			lsp_zero.default_keymaps({ buffer = bufnr })
-		end)
+			})
+			lsp_zero.extend_lspconfig()
+			lsp_zero.on_attach(function(_, bufnr)
+				lsp_zero.default_keymaps({ buffer = bufnr, preserve_mappings = false })
+			end)
 
-		require("mason").setup({})
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"astro",
-				"eslint",
-				"gopls", -- go
-				"lua_ls",
-				"prettier",
-				"rust_analyzer",
-				"svelte",
-				"tsserver", -- typescript
-				"volar", -- vue
-			},
-			handlers = {
-				lsp_zero.default_setup,
-			},
-		})
-	end,
+			require("mason").setup({})
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"astro",
+					"eslint",
+					"gopls", -- go
+					"lua_ls",
+					"prettier",
+					"rust_analyzer",
+					"svelte",
+					"volar", -- vue
+				},
+				handlers = {
+					lsp_zero.default_setup,
+				},
+			})
+		end,
+	},
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		config = true,
+	},
 }
