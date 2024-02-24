3ad5b487a3e9df8caba817bef7a3a26c85da1332
Emanuel Pilz <emonadeo@gmail.com>
2024-02-24 01:53:41 +0100

    remove tokyonight opts

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,13 +3,6 @@ return {
 	{
 		"folke/tokyonight.nvim",
 		priority = 1000,
-		opts = {
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
-			},
-		},
 	},
 
 	-- catppuccin
