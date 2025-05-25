fc424e6efc513646a7fef03ab432f5cda785bd61
Emanuel Pilz <emonadeo@gmail.com>
2025-05-25 22:46:48 +0200

    add copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,18 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	opts = {
+		panel = { enabled = false },
+		suggestion = { enabled = true, auto_trigger = false },
+	},
+	config = function(_, opts) require("copilot").setup() end,
+	keys = {
+		{
+			"<C-;>",
+			function() require("copilot.suggestion").accept() end,
+			mode = "i",
+			desc = "Trigger Copilot",
+		},
+	},
+}
